f4a190782532aae071cd070c341fa22d9db5a62a
Thomaslorentzen <thomas.lorentzen@hotmail.com>
2021-03-15 16:42:05 +0100

    Implementeret skelet til shifting af grid

--- cards.py
+++ cards.py
@@ -1,4 +1,61 @@
+from numpy.core import number
 
-print("hej")
+discard_pile = []
 
-print("Test")
+cards = []
+
+
+def main():
+    shift_grid()
+
+
+def move():
+    pass
+
+
+def shift_grid():
+
+    while True:
+        grid = input("Do you want to shift {L}eft, {R}ight or {M}iddle grid? (x to exit) ")
+        if grid.upper() == "L":
+            direction_left = input("Press 1 to shift up, 0 to shift down")
+            if direction_left == "1":
+                print("Shifting left grid upwards")
+            elif direction_left == "0":
+                print("Shifting left grid downwards")
+            elif direction_left == "x":
+                print("Ending turn")
+                break
+            else:
+                print("Invalid input! Try again!")
+        elif grid.upper() == "R":
+            direction_right = input("Press 1 to shift up, 0 to shift down")
+            if direction_right == "1":
+                print("Shifting right grid upwards")
+            elif direction_right == "0":
+                print("Shifting right grid downwards")
+            elif direction_right == "x":
+                print("Ending turn")
+                break
+            else:
+                print("Invalid input! Try again!")
+        elif grid.upper() == "M":
+            direction_mid = input("Press 1 to shift up, 0 to shift down")
+            if direction_mid == "1":
+                print("Shifting middle grid upwards")
+            elif direction_mid == "0":
+                print("Shifting middle grid downwards")
+            elif direction_mid == "x":
+                print("Ending turn")
+                break
+            else:
+                print("Invalid input! Try again!")
+        elif grid.upper() == "x":
+            print("Exiting shifting")
+            break
+        else:
+            print("Invalid input! Try again!")
+
+
+if __name__ == '__main__':
+    main()
