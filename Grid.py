d984b0626480d227101058456830ab74f7b806bc
Thomaslorentzen <thomas.lorentzen@hotmail.com>
2021-03-27 16:01:03 +0100

    Grid virker nu

--- Grid.py
+++ Grid.py
@@ -1,9 +1,22 @@
-class Board:
+import BlockingCars
+from HeroCars import HeroCars
+from BlockingCars import BlockingCars
+import random
 
-    board_x = 15
+
+class Car:
+    def __init__(self, id, X_coord, Y_coord, direction):
+        self.id = id
+        self.direction = direction
+        self.Y_coord = Y_coord
+        self.X_coord = X_coord
+
+
+class Board:
+    board_x = 14
     board_y = 16
 
-    #The different grids that can be shifted with the shift card. Start and end positions for the loops
+    # The different grids that can be shifted with the shift card. Start and end positions for the loops
 
     Grid1_x_start = 0
     Grid1_x_end = 4
@@ -11,96 +24,79 @@ class Board:
     Grid1_y_end = 11
 
     Grid2_x_start = 5
-    Grid2_x_end = 9
+    Grid2_x_end = 8
     Grid2_y_start = 5
     Grid2_y_end = 11
 
-    Grid3_x_start = 10
+    Grid3_x_start = 9
     Grid3_x_end = 14
     Grid3_y_start = 5
     Grid3_y_end = 11
 
+    Carlist = []
+
     Board = []
-    pair = (0,0)
-        
+
+    cars_to_place = 10
+
+    start_pos1_x = 0
+    start_pos1_y = 7
+
+    start_pos2_x = 13
+    start_pos2_y = 7
+
+
+
+    # should be its own function to initializee board.
+
+
+
     for Row in range(board_x):
-        Board.append([pair])
+        Board.append(["", 0])
         for Column in range(board_y):
-          Board[Row].append([pair])
+            Board[Row].append(["", 0])
 
     for Row in range(Grid1_x_start, Grid1_x_end):
-        for Column in range(Grid1_y_start,Grid1_y_end):
-            Board[Row][Column] = [(0,1)]
+        for Column in range(Grid1_y_start, Grid1_y_end):
+            Board[Row][Column] = ["", 1]
 
     for Row in range(Grid2_x_start, Grid2_x_end):
-        for Column in range(Grid2_y_start,Grid2_y_end):
-            Board[Row][Column] = [(0,2)]
+        for Column in range(Grid2_y_start, Grid2_y_end):
+            Board[Row][Column] = ["", 2]
 
     for Row in range(Grid3_x_start, Grid3_x_end):
-        for Column in range(Grid3_y_start,Grid3_y_end):
-            Board[Row][Column] = [(0,3)]
+        for Column in range(Grid3_y_start, Grid3_y_end):
+            Board[Row][Column] = ["", 3]
 
-#  These functions are shifting the grid. They need to first remark the fields that was the original grid to 0.
-#  Then It will mark the fields that now mark the grids new position with the numbers 1,2 or 3 depending on the grid. 
-#  Finaly it needs to check for car placements on the old grid and then save thosee to put into the new grid. 
+    Board[start_pos1_x][start_pos1_y][0] = "H1"
+    Board[start_pos2_x][start_pos2_y][0] = "H2"
 
-    def shiftGrid(self,Grid,shift):
-# Check if grid is valid
-        if Grid not in range(1,3):
-            print("grid not valid. Pick a number between 1 and 3")
+    while cars_to_place > 0:
+        random_x = random.randrange(Grid1_x_start, Grid3_x_end)
+        random_y = random.randrange(Grid1_y_start, Grid3_y_end)
+        direction = "vertical"
+        if not Board[random_x][random_y][0]:
+            print("Du er i if statement chap, med " + str(cars_to_place) + " biler at skulle placere din nar")
+            print("{} and {} are our coordinates".format(random_x, random_y))
+            car = Car("c" + str(cars_to_place), random_x, random_y, direction)
+            Board[random_x][random_y][0] = car.id
+            cars_to_place = cars_to_place - 1
+        else:
+            continue
 
-#  Check the if the shift is valid. Remmeber to expand this to the new positions of the grids after a shift. Validity should rely on the new x positions of the grid. 
-        if shift not in range(-5,5) :
-            print("shift is out of bounds, make sure shift is within the board")
-
-#  TODO: Find car positions
-
-#  Removes the old grid
-        elif Grid == 1:
-
-          for Row in range(self.Grid1_x_start, self.Grid1_x_end):
-              for Column in range(self.Grid1_y_start,self.Grid1_y_end):
-                self.Board[Row][Column] = [(0,0)]
-
-#  Marks the new grid 
-          for Row in range(self.Grid1_x_start, self.Grid1_x_end):
-            for Column in range(self.Grid1_y_start+shift,self.Grid1_y_end+shift):
-                self.Board[Row][Column] = [(0,1)]
-
-#  Removes the old grid
-        elif Grid == 2:
-
-          for Row in range(Grid2_x_start, Grid2_x_end):
-              for Column in range(self.Grid1_y_start,self.Grid1_y_end):
-                self.Board[Row][Column] = [(0,0)]
-
-#  Marks the new grid 
-          for Row in range(self.Grid2_x_start, self.Grid2_x_end):
-            for Column in range(self.Grid1_y_start+shift,self.Grid1_y_end+shift):
-                self.Board[Row][Column] = [(0,1)]
+    #  These functions are shifting the grid. They need to first remark the fields that was the original grid to 0.
+    #  Then It will mark the fields that now mark the grids new position with the numbers 1,2 or 3 depending on the grid.
+    #  Finaly it needs to check for car placements on the old grid and then save thosee to put into the new grid.
 
 
-#  Removes the old grid
-        elif Grid == 3:
 
-          for Row in range(self.Grid3_x_start, self.Grid3_x_end):
-              for Column in range(self.Grid1_y_start,self.Grid1_y_end):
-                self.Board[Row][Column] = [(0,0)]
-
-#  Marks the new grid 
-          for Row in range(self.Grid3_x_start, self.Grid3_x_end):
-            for Column in range(self.Grid1_y_start+shift,self.Grid1_y_end+shift):
-                self.Board[Row][Column] = [(0,1)]
-
-
+# tests
 Board1 = Board()
 print("Board1 before shift:")
 print(Board1.Board)
 
-Board1.shiftGrid(1, 4)
+#Board1.shiftGrid(1, 4)
 
-print("Board1 after shift: ")
+car = HeroCars()
+print("Board1 after shift: Jeg er sej")
 print(Board1.Board)
-
-
-
