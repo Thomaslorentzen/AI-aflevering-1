5b1410888fe5b68d0f17da59a864c2eb0c2c84f0
Thomaslorentzen <thomas.lorentzen@hotmail.com>
2021-03-28 15:22:50 +0200

    Nye ændrigner med grid

--- Grid.py
+++ Grid.py
@@ -5,6 +5,10 @@ import random
 from pprint import pprint
 
 
+# Vi skal få X og Y koordinaterne i bilklassen til at stemme overens med hvor de placeres på boardet
+# TYPE not subscriptable - hvad betyder det?
+#
+
 class Car:
     def __init__(self, id, X_coord, Y_coord, direction):
         self.id = id
@@ -26,12 +30,12 @@ class Board:
     # The different grids that can be shifted with the shift card. Start and end positions for the loops
 
     Grid1_x_start = 0
-    Grid1_x_end = 4
+    Grid1_x_end = 5
     Grid1_y_start = 5
     Grid1_y_end = 11
 
     Grid2_x_start = 5
-    Grid2_x_end = 8
+    Grid2_x_end = 9
     Grid2_y_start = 5
     Grid2_y_end = 11
 
@@ -40,84 +44,121 @@ class Board:
     Grid3_y_start = 5
     Grid3_y_end = 11
 
-    car_list = []
+    car_list = {
+
+    }
 
     Board = []
 
     cars_to_place = 10
 
+    start_pos1_y = 8
     start_pos1_x = 0
-    start_pos1_y = 7
 
-    start_pos2_x = 13
     start_pos2_y = 7
+    start_pos2_x = 13
+
+    def print_board(self):
+        for row in self.Board:
+            print(row)
+        # print(self.Board)
+
+        # for i in self.car_list.keys():
+        #    print(self.car_list.values())
+
+        print("Our direction for c1 is {}".format(Board.car_list["c1"].direction))
+        print("our x-coordinates for c1 are {}".format(Board.car_list["c1"].X_coord))
+        print("our y coordinates for c1 are {}".format(Board.car_list["c1"].Y_coord))
 
     def initiate_board(self):
-        for Row in range(self.board_x):
-            self.Board.append(["", 0])
-            for Column in range(self.board_y):
-                self.Board[Row].append(["", 0])
 
-        for Row in range(self.Grid1_x_start, self.Grid1_x_end):
-            for Column in range(self.Grid1_y_start, self.Grid1_y_end):
-                self.Board[Row][Column] = ["", 1]
+        self.Board = [[["", 0] for i in range(self.board_x)] for j in range(self.board_y)]
+        for i in range(self.Grid1_y_start, self.Grid1_y_end):
+            for j in range(self.Grid1_x_start, self.Grid1_x_end):
+                self.Board[i][j] = ["", 1]
 
-        for Row in range(self.Grid2_x_start, self.Grid2_x_end):
-            for Column in range(self.Grid2_y_start, self.Grid2_y_end):
-                self.Board[Row][Column] = ["", 2]
+        for i in range(self.Grid2_y_start, self.Grid2_y_end):
+            for j in range(self.Grid2_x_start, self.Grid2_x_end):
+                self.Board[i][j] = ["", 2]
 
-        for Row in range(self.Grid3_x_start, self.Grid3_x_end):
-            for Column in range(self.Grid3_y_start, self.Grid3_y_end):
-                self.Board[Row][Column] = ["", 3]
+        for i in range(self.Grid3_y_start, self.Grid3_y_end):
+            for j in range(self.Grid3_x_start, self.Grid3_x_end):
+                self.Board[i][j] = ["", 3]
 
-        self.Board[self.start_pos1_x][self.start_pos1_y][0] = "H1"
-        self.Board[self.start_pos2_x][self.start_pos2_y][0] = "H2"
+        self.Board[self.start_pos1_y][self.start_pos1_x][0] = "H1"
+        self.Board[self.start_pos2_y][self.start_pos2_x][0] = "H2"
 
         while self.cars_to_place > 0:
 
             random_x = random.randrange(self.Grid1_x_start, self.Grid3_x_end)
             random_y = random.randrange(self.Grid1_y_start, self.Grid3_y_end)
-            direction = "vertical"
-            if not self.Board[random_x][random_y][0]:
+            direction = ""
+            is_occupied = self.Board[random_x][random_y][0] == ""
+            # print("value: {}".format(is_occupied))
+            # print("{}".format(self.cars_to_place))
+            # print("{} and {} are our coordinates".format(random_x, random_y))
+            if self.Board[random_x][random_y][0] == "":
                 print("Du er i if statement chap, med " + str(self.cars_to_place) + " biler at skulle placere din nar")
                 print("{} and {} are our coordinates".format(random_x, random_y))
-                car = Car("c" + str(self.cars_to_place), random_x, random_y, direction)
+                # car = Car("c" + str(self.cars_to_place), random_x, random_y, direction)
                 if random.randrange(0, 2) == 0:
-                    car.direction = "vertical"
+                    direction = "vertical"
                 else:
-                    car.direction = "horizontal"
-                self.Board[random_x][random_y][0] = car.id
+                    direction = "horizontal"
+                car = Car("c" + str(self.cars_to_place), random_x, random_y, direction)
+                self.Board[random_y][random_x][0] = ["c" + str(self.cars_to_place)]
                 self.cars_to_place = self.cars_to_place - 1
-                self.car_list.append(car)
+                self.car_list["c" + str(self.cars_to_place)] = car
             else:
                 continue
-        for obj in self.car_list:
-            print(obj.id, obj.direction)
+        # for obj in self.car_list:
+
+    #            print(obj.id, obj.direction)
+
+    # print(self.car_list.keys())
+    # print(self.car_list.values())
 
     #  These functions are shifting the grid. They need to first remark the fields that was the original grid to 0.
     #  Then It will mark the fields that now mark the grids new position with the numbers 1,2 or 3 depending on the grid.
     #  Finaly it needs to check for car placements on the old grid and then save thosee to put into the new grid.
 
-    def move_car(self, car, movement_num, Board, direction):
+    def move_car(self, car, movement_num, direction):
         self.car = car
         self.movement_num = movement_num
-        self.board = Board
         self.direction = direction
 
-        if car.id in Board.car_list:
-            if car.direction == "vertical":
+        if self.car in Board.car_list.keys():
+            if Board.car_list[self.car].direction == "vertical":
                 if self.direction == "up":
-                    if car.Y_coord - movement_num < 5:
-                        return False
-                    if not Board[car.X_coord][car.Y_coord - movement_num][0]:
-                        return False
-                    else:
-                        Board[car.X_coord][car.Y_coord][0] = ""
-                        Board[car.X_coord][car.Y_coord - movement_num][0] = car.id
-                        car.Y_coord = car.Y_coord - movement_num
+                    test_i_board = Board.car_list[self.car].Y_coord - movement_num < 5
+                    print("Værdien er {}".format(test_i_board))
+                    if not Board.car_list[self.car].Y_coord - movement_num < 5:
+                        test = self.Board[Board.car_list[self.car].Y_coord - movement_num][Board.car_list[self.car].X_coord][0] == ""
+                        print("Der er ikke nogen bil her")
+                        print("koordinaterne er {}, {}".format(Board.car_list[self.car].X_coord, Board.car_list[self.car].Y_coord))
 
-        if car.id in Board.car_list:
-            if car.direction == "vertical":
+                        return False
+                    if self.Board[Board.car_list[self.car].Y_coord
+                                  - movement_num][Board.car_list[self.car].X_coord][0] == "":
+                        print("Du er helt inde nu!")
+
+                        self.Board[Board.car_list[self.car].Y_coord][Board.car_list[self.car].X_coord] = ""
+                        self.Board[Board.car_list[self.car].Y_coord - movement_num][Board.car_list \
+                            [self.car].X_coord][0] = Board.car_list[self.car].id
+                        # self.Board.car_list[self.car.Y_coord - movement_num][self.car.X_coord][0] = Board.car_list[self.car].id
+                        Board.car_list[self.car].Y_coord = Board.car_list[self.car].Y_coord - movement_num
+                        print("Vi er i else statement nu!")
+                    else:
+                        pass
+                        #self.Board[Board.car_list[self.car].Y_coord][Board.car_list[self.car].X_coord] = ""
+                        #self.Board[Board.car_list[self.car].Y_coord - movement_num][Board.car_list\
+                        #[self.car].X_coord][0] = Board.car_list[self.car].id
+                        #self.Board.car_list[self.car.Y_coord - movement_num][self.car.X_coord][0] = Board.car_list[self.car].id
+                        #Board.car_list[self.car].Y_coord = Board.car_list[self.car].Y_coord - movement_num
+                        #print("Vi er i else statement nu!")
+
+        if self.car in Board.car_list:
+            if Board.car_list[self.car].direction == "vertical":
                 if self.direction == "down":
                     if car.Y_coord + movement_num > 11:
                         return False
@@ -128,8 +169,8 @@ class Board:
                         Board[car.X_coord][car.Y_coord + movement_num][0] = car.id
                         car.Y_coord = car.Y_coord + movement_num
 
-        if car.id in Board.car_list:
-            if car.direction == "horizontal":
+        if self.car in Board.car_list:
+            if Board.car_list[self.car].direction == "horizontal":
                 if self.direction == "left":
                     if car.X_coord - movement_num < 0:
                         return False
@@ -140,8 +181,8 @@ class Board:
                         Board[car.X_coord - movement_num][car.Y_coord][0] = car.id
                         car.X_coord = car.X_coord - movement_num
 
-        if car.id in Board.car_list:
-            if car.direction == "horizontal":
+        if self.car in Board.car_list:
+            if Board.car_list[self.car].direction == "horizontal":
                 if self.direction == "right":
                     if car.X_coord + movement_num > 13:
                         return False
@@ -152,28 +193,14 @@ class Board:
                         Board[car.X_coord + movement_num][car.Y_coord][0] = car.id
                         car.X_coord = car.X_coord + movement_num
 
-    def print_board(self):
-        for x in Board.board_x:
-            for y in Board.board_y:
-                print("{:4}".format(y),
-                      print())
-
-        # for x in range(self.board_x):
-        #   for y in range(self.board_y):
-        #      print(x, y,  end = " ")
-        #     print()
-        # print("Koordinaterne x og y er {} og {}".format(x, y))
-        # pprint(self.Board[x][y])
-
 
 # tests
 Board1 = Board()
-print("Board1 before shift:")
-Board1.print_board()
-# pprint(Board1)
-# Board1.shiftGrid(1, 4)
+print("initiate board")
+
 Board1.initiate_board()
-car = HeroCars()
-# Board1.move_car()
-print("Board1 after shift: Jeg er sej")
-print(Board1.Board)
+
+Board1.print_board()
+# print("vi forsøger at bevæge bilen for helvede")
+Board1.move_car("c1", 1, "up")
+Board1.print_board()
